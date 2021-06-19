21713ce2cd0a17861445a2bfe1273146a6874e49
ben leie <awecg@qq.com>
2021-06-19 15:11:37 +0800

    <js> 知乎闭包

--- interview/doit/closure.js
+++ interview/doit/closure.js
@@ -7,15 +7,15 @@ function Add3(){
   let a = 10;
   return function(){
       a++;
-      console.log(a)
+    //   console.log(a)
       return a;
   };
 };
 var cc =  Add3();
 cc()
 cc()
-cc()
-console.log(a);
+// console.log(cc())
+// console.log(a);
 
 
 /* function Add2(){
@@ -55,3 +55,19 @@ console.log(moduleA.flag = false)
 moduleA.minus()
 moduleA.minus()
 console.log(moduleA) */
+
+
+// https://zhuanlan.zhihu.com/p/25855075
+/* for (var i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(new Date, i);
+    }, 1000);
+}
+console.log(new Date, i);
+ */
+for(var i = 0; i < 5; i++) {
+    (function(j) { // j=i
+        setTimeout(function(){console.log(new Date, j)}, 1000)
+    })(i)
+}
+console.log(new Date, i)
