b43063843e26b0ecc4766203cb0a906531808ac0
ben leie <awecg@qq.com>
2021-06-23 17:39:25 +0800

    <es6> promise 45: then,catch,finally

--- interview/doit/promise.js
+++ interview/doit/promise.js
@@ -119,8 +119,9 @@ setTimeout(() => {
   console.log('timer2')
 }, 0)
 console.log('start')
-
 // timer1  ---  timer1 end --- promise -- timer2
+
+
 
 Promise.resolve().then(() => {
   console.log('promise1');
@@ -136,10 +137,254 @@ const timer1 = setTimeout(() => {
 }, 0)
 console.log('start');
 // start -- promise1 -- timer1 -- promise2 -- timer2
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  }, 1000)
+})
+const promise2 = promise1.then(() => {
+  throw new Error('error!!!')
+})
+console.log('promise1', promise1)
+console.log('promise2', promise2)
+setTimeout(() => {
+  console.log('promise1', promise1)
+  console.log('promise2', promise2)
+}, 2000)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+ 3.Promise中的then、catch、finally
+
+const promise = new Promise((resolve, reject) => {
+  resolve("success1");
+  reject("error");
+  resolve("success2");
+});
+promise
+.then(res => {
+    console.log("then: ", res);   // then:  success1
+  }).catch(err => {
+    console.log("catch: ", err);
+  })
+
+
+
+const promise = new Promise((resolve, reject) => {
+  reject("error");
+  resolve("success2");
+});
+promise
+.then(res => {
+    console.log("then1: ", res);
+  }).then(res => {
+    console.log("then2: ", res);
+  }).catch(err => {
+    console.log("catch: ", err);    //error
+  }).then(res => {
+    console.log("then3: ", res);   //undefined
+  })
+
+
+
+Promise.resolve(1)
+  .then(res => {
+    console.log(res);   // 1
+    return 2;
+  })
+  .catch(err => {
+    return 3;
+  })
+  .then(res => {
+    console.log(res);   // 2
+  });
+
+  
+Promise.reject(1)
+  .then(res => {
+    console.log(res);
+    return 2;
+  })
+  .catch(err => {
+    console.log(err);   // 1
+    return 3
+  })
+  .then(res => {
+    console.log(res);   // 3
+  });
+
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('timer')
+    resolve('success')
+  }, 1000)
+})
+const start = Date.now();
+promise.then(res => {
+  console.log(res, Date.now() - start)
+  // return 1
+})
+promise.then(res => {
+  console.log(res, Date.now() - start)  //success  1014
+})
+
+
+
+Promise.resolve().then(() => {
+  return new Error('error!!!')
+}).then(res => {
+  console.log("then: ", res)   // then:  Error: error!!!
+}).catch(err => {
+  console.log("catch: ", err)
+})
+
+
+const promise = Promise.resolve().then(() => {
+  return promise;   //TypeError: Chaining cycle detected for promise
+})
+promise.catch(console.err)
+
+
+Promise.resolve(1)
+  .then(2)                      // 传入非函数则会发生值透传
+  .then(Promise.resolve(3))
+  .then(console.log)            //  1 
+
+
+
+
+Promise.reject('err!!!')
+  .then((res) => {
+    console.log('success', res)
+  }, (err) => {
+    console.log('error', err)   // error errr!!!
+  }).catch(err => {
+    console.log('catch', err)
+  })
+
+// 如果把第二个参数去掉，就进入了catch()
+Promise.reject('error!!!')
+  .then((res) => {
+    console.log('success', res)
+  }).catch(err => {
+    console.log('catch', err)   // catch error!!!
+  })
+
+
+Promise.resolve()
+  .then(function success (res) {
+    throw new Error('error!!!')
+  }, function fail1 (err) {
+    console.log('fail1', err)
+  }).catch(function fail2 (err) {
+    console.log('fail2', err)    // fail2 Error: error!!!
+  })
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+Promise.resolve('1')
+  .then(res => {
+    console.log(res)
+  })
+  .finally(() => {
+    console.log('finally')
+  })
+Promise.resolve('2')
+  .finally(() => {
+    console.log('finally2')
+  	return '我是finally2返回的值'
+  })
+  .then(res => {
+    console.log('finally2后面的then函数', res)
+  })
+
+
+  Promise.resolve('1')
+  .finally(() => {
+    console.log('finally1')
+    // throw new Error('我是finally中抛出的异常')
+    // return new Error('我是finally中抛出的异常')
+  })
+  .then(res => {
+    console.log('finally后面的then函数', res)
+  })
+  .catch(err => {
+    console.log('捕获错误', err)
+  })
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// 链式调用的then和finally需要等当前微任务执行结束后，才能加入微任务队列
+
+function promise1 () {
+  let p = new Promise((resolve) => {
+    console.log('promise1');
+    resolve('1')
+  })
+  return p;
+}
+function promise2 () {
+  return new Promise((resolve, reject) => {
+    reject('error')
+  })
+}
+promise1()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .finally(() => console.log('finally1'))
+promise2()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .finally(() => console.log('finally2'))
+// promise1 --- 1 ---  error  --- finally1 --- finally2
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
 */
+
+function promise1 () {
+  return new Promise((resolve) => {
+    console.log('promise1');
+    resolve('1')
+  })
+}
+function promise2 () {
+  return new Promise((resolve, reject) => {
+    reject('error')
+  })
+}
+promise1()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .then(() => console.log('finally1'))
+  .then(() => console.log('finally3'))
+
+promise2()
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  .then(() => console.log('finally2'))
+
+
+
+
+
+
+
+
+// 4.--------------------------------------------------------------------------------------- //
+
+
+
+
+
+
+
 
 
 
