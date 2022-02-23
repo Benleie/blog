db558a2abf3c5b22043c49e382575ac412cae192
Ben Lee <awecg@qq.com>
2022-02-23 09:34:58 +0800

    <review> 整理笔试题

--- interview/interview.js
+++ interview/interview.js
@@ -21,21 +21,6 @@ var b = 1
 
 const c = 2
 
-
-console.log(1)
-setTimeout(() => {
-    console.log(2)
-}, 0)
-new Promise((resolve) => {
-    console.log(3)
-    resolve()
-}).then(() => {
-    console.log(4)
-})
-console.log(5)
-
-
-
 const Staff = (name) => {
     this.name = name
 }
@@ -51,36 +36,23 @@ prototype  constructor  proto
 
 
 function do1 (n) {
-
    let i = 0
-
    while (i < n) {
-
        i++
-
    }
-
 }
-
 function do2 (n) {
-
    let i = 1
-
    while (i <= n) {
-
        i = i * 2
-
       do1(n)
-
    }
 }
 */
 
 
 /*function Staff (name) {
-
     this.name = name
-
 }
 
 
@@ -93,18 +65,29 @@ console.log()*/
 var name = 'chris'
 var staffA = {
     name: 'hunter',
+    getName: function() { console.log(this.name) },
+    getName2: () => { console.log(this.name) },
     getShowName: function () {
         return function () { console.log(this.name)
         }
     }
 }
+staffA.getName()   //hunter
+staffA.getName.call(name)   //hunter
+staffA.getName2()   //hunter
 staffA.getShowName()()
+const sayName = new staffA.getName()
+// sayName()
+const sayShowName = new staffA.getShowName()
+sayShowName()
 
+/* setTimeout Promise
 
 console.log(1)
 setTimeout(() => {
     console.log(2)
     Promise.resolve().then(() => {console.log(3)})
+    console.log("A")
 }, 0)
 new Promise((resolve) => {
     console.log(4)
@@ -120,4 +103,6 @@ new Promise((resolve) => {
     console.log(8)
 })
 console.log(9)
-// 1 4 9 7 8 2 3 5 6
+// 1 4 9 7 8
+// 2 A 3 5 6
+*/
